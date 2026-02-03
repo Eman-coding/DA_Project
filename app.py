2ee104908c6548a5be8430de6f340b4da89d8960
Eman <emaan071104@gmail.com>
2026-02-03 15:03:12 +0500

    new change

--- app.py
+++ app.py
@@ -114,3 +114,4 @@ def predict():
 if __name__ == '__main__':
     print("Starting Flask app...")
     app.run(debug=True, host='127.0.0.1', port=5000, use_reloader=False)
+# this is the end of the file
